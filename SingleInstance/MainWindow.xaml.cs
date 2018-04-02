299590c08988c7fb64fadc7890e66ecc79b212d7
Jair Pereira <37883761+jairxpereira@users.noreply.github.com>
2018-04-01 22:13:22 -0300

    Testando a classe Application
    Teste da classe Application para produzir aplicações de instância única.

---
+++ SingleInstance/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SingleInstance
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Window> janelas = null;
+        private int janelaIndex;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            janelas = new List<Window>();
+            janelaIndex = 0;
+
+
+
+        }
+
+        public void addWindow( Window wnd )
+        {
+            janelas.Add(wnd);
+            listJanelas.Items.Add(wnd.Title);
+            janelaIndex++;
+
+
+        }
+
+        public List<Window> getJanelas()
+        {
+            return janelas;
+        }
+
+        private void btnNovaJanela_onClick(object sender, RoutedEventArgs e)
+        {
+            Window wnd = new WndEditor();
+            wnd.Title = "Janela_" + janelaIndex.ToString();
+            wnd.Owner = this;
+            addWindow(wnd);
+            wnd.Show();
+        }
+    }
+}
