f6c252f6b8812d0f8b738fe2323e23e61b63b83a
Jair Pereira <37883761+jairxpereira@users.noreply.github.com>
2018-04-14 17:54:43 -0300

    Teste de resources: imagens em uma dll externa

---
+++ ImageLibrary/Tools.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+
+namespace ImageLibrary
+{
+    public class Tools
+    {
+        public static void sayHello( string msg)
+        {
+
+            if ( msg ==null || msg == "")
+            MessageBox.Show("Sucess! This is a hello from a lib", "Lib: ImageLibrary.dll");
+            else
+                MessageBox.Show( 
+                    msg, "Lib: ImageLibrary.dll");
+
+        }
+    }
+}
--- WPFLab/MainWindow.xaml.cs
+++ WPFLab/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +15,22 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ImageLibrary;
 
-namespace WPFLab
+
+namespace WPFLab 
 {
     /// <summary>
     /// Interação lógica para MainWindow.xam
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool bUriRelative;
+
         public MainWindow()
         {
             InitializeComponent();
+            bUriRelative = true;
         }
 
         private void btnWindowText_onClick(object sender, RoutedEventArgs e)
@@ -85,6 +93,70 @@ namespace WPFLab
         {
             var wnd = new Janelas.WndCommand();
             wnd.Show();
+        }
+
+        // btnGetResourceList
+        private void btnGetResourceList_onClick(object sender, RoutedEventArgs e)
+        {
+
+            Assembly assembly = Assembly.GetAssembly(this.GetType());
+            string resourceName = assembly.GetName().Name + ".g";
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager(resourceName, assembly);
+            using (ResourceSet set =
+            rm.GetResourceSet(System.Globalization.CultureInfo.CurrentCulture, true, true))
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (DictionaryEntry res in set)
+                {
+                    String str = res.Key.ToString() + "\n\r";
+                    sb.Append(str );
+
+                }
+
+                Uri uri = null;
+                if (bUriRelative == true)
+                uri = new Uri("/Images/Pic2.png", UriKind.Relative);
+                else
+                    uri = new Uri("pack://application:,,,/Images/Pic0.png");
+
+                bUriRelative = !bUriRelative;
+
+                sb.Append(uri.ToString());
+
+                img.Source = new BitmapImage(uri);                
+                Sound.Play();
+                MessageBox.Show(sb.ToString(), resourceName);
+                
+
+
+            }
+
+        }
+
+        private void ResourceOtherLib_onClick(object sender, RoutedEventArgs e)
+        {
+            Uri uri = null;
+            if (bUriRelative == true)
+            {
+                uri = new Uri("ImageLibrary;component/images/Pic0.png", UriKind.Relative);
+                img.Source = new BitmapImage(uri);
+                Tools.sayHello("Uri relativa");
+            }
+                
+            else
+            {
+                uri = new Uri("pack://application:,,,/ImageLibrary;component/images/Pic1.png");
+                img.Source = new BitmapImage(uri);
+                Tools.sayHello("Uri absoluta");
+            }
+                
+
+            
+
+            bUriRelative = !bUriRelative;
+
+            // new Uri("ImageLibrary;component/images/winter.jpg", UriKind.Relative));
+           
         }
     }
 }
